28d1f318dd0fc6948414bea6416905604f6cd461
marcinfabisiak97 <marcinfab1@o2.pl>
2022-02-04 18:20:37 +0100

    correcting form

--- src/components/main-wrapper_contact/index.js
+++ src/components/main-wrapper_contact/index.js
@@ -1,26 +1,73 @@
 import kontaktikona from '../../assets/kontakt_ikona.png';
 import zdj1 from '../../assets/zdj1.jpg';
 import React, { useState } from 'react';
+import { send } from 'emailjs-com';
 const Mainwrappercontact = ({ aboutref, skillref, myworkref, blogref, contactref }) => {
+    const onSubmit = (e) => {
+        e.preventDefault();
+        send(
+            'service_angvk1e',
+            'template_7othq2o',
+            toSend,
+            'user_CZXASdETJkJ7zZ1G1Ouhg'
+        )
+            .then((response) => {
+                console.log('SUCCESS!', response.status, response.text);
+            })
+            .catch((err) => {
+                console.log('FAILED...', err);
+            });
+    };
+    const [toSend, setToSend] = useState({
+        from_name: '',
+        to_name: '',
+        message: '',
+        reply_to: '',
+    });
+    const handleChange = (e) => {
+        setToSend({ ...toSend, [e.target.name]: e.target.value });
+    };
 
     return (
         <div className="main-wrapper_contact" ref={contactref}>
             <div className="main-wrapper_contactparent">
                 <h2 id="contact">//Contact me</h2>
                 <p>If you are willing to work with me, contact me. I can join your conference to serve you with engeneering experience.</p>
-                <div className="main-wrapper_contactchild1">
-                    <input size="40px" type="email" name="" id="email" placeholder="Your E-mail" required />
-                </div>
-                <div className="main-wrapper_contactchild2">
-                    <input type="email" name="" id="name" placeholder="Your name" required />
-                </div>
-                <div className="main-wrapper_contactchild3">
-                    <textarea placeholder='How can I help you? &#13;Please,put here your meassage/reguest'></textarea>
-                </div>
-            </div>
-            <div className="main-wrapper_contactchild">
-                <input type="button" name="" value="send" />
-            </div>
+                <form onSubmit={onSubmit}>
+                    <div className="main-wrapper_contactchild1">
+                        <input
+                            type='text'
+                            name='reply_to'
+                            placeholder='Your email'
+                            value={toSend.reply_to}
+                            onChange={handleChange}
+                        />
+                    </div>
+                    <div className="main-wrapper_contactchild2">
+                        <input
+                            type='text'
+                            name='from_name'
+                            placeholder='Your name'
+                            value={toSend.from_name}
+                            onChange={handleChange}
+                        />
+
+                    </div>
+                    <div className="main-wrapper_contactchild3">
+                        <textarea
+                            type='text'
+                            name='message'
+                            placeholder='How can I help you? &#13;Please,put here your meassage/reguest.'
+                            value={toSend.message}
+                            onChange={handleChange}
+                        >
+                        </textarea>
+                    </div>
+                    <div className="main-wrapper_contactchild">
+                        <button type='submit'>Submit</button >
+                    </div>
+                </form>
+            </div >
             <div className="main-info_contact">
                 <img src={kontaktikona} />
                 <div className="main-info_contactdesc">
@@ -37,7 +84,8 @@ const Mainwrappercontact = ({ aboutref, skillref, myworkref, blogref, contactref
                 <p>git: https://github.com/marcinfabisiak97</p>
 
             </div>
-        </div>
+
+        </div >
     )
 }
 export default Mainwrappercontact;
