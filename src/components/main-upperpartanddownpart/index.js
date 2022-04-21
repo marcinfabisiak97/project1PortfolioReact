c8146e456deb9b12d9a8dd1d63d2c03a7bfd2032
marcinfabisiak97 <marcinfab1@o2.pl>
2022-04-21 20:17:14 +0200

    changing a href range

--- src/components/main-upperpartanddownpart/index.js
+++ src/components/main-upperpartanddownpart/index.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import github from '../../assets/github_icon.png';
-import cv from '../../assets/cv.pdf';
-import { useState, useRef, useEffect } from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
+import github from "../../assets/github_icon.png";
+import cv from "../../assets/cv.pdf";
+import { useState, useRef, useEffect } from "react";
 const Upperpartanddownpart = ({ skillref, aboutref }) => {
     const [scrolled, setScrolled] = useState(false);
     const dref = useRef(null);
@@ -19,29 +19,31 @@ const Upperpartanddownpart = ({ skillref, aboutref }) => {
         window.addEventListener("scroll", handleScroll);
     });
     const myBar = [
-        { 'styl': '60%', 'klas': 'bar-javascript', 'text': 'javascript 60%' },
-        { 'styl': '60%', 'klas': 'bar-react', 'text': 'react 60%' },
-        { 'styl': '40%', 'klas': 'bar-typescript', 'text': 'typescript 40%' },
-        { 'styl': '80%', 'klas': 'bar-css', 'text': 'css 80%' },
-        { 'styl': '100%', 'klas': 'bar-html', 'text': 'html 100%' },
-        { 'styl': '20%', 'klas': 'bar-node', 'text': 'node 20%' },
-    ]
+        { styl: "60%", klas: "bar-javascript", text: "javascript 60%" },
+        { styl: "60%", klas: "bar-react", text: "react 60%" },
+        { styl: "40%", klas: "bar-typescript", text: "typescript 40%" },
+        { styl: "80%", klas: "bar-css", text: "css 80%" },
+        { styl: "100%", klas: "bar-html", text: "html 100%" },
+        { styl: "20%", klas: "bar-node", text: "node 20%" },
+    ];
     const ButtonMailto = ({ mailto }) => {
         return (
             <React.Fragment>
                 <Link
-                    to='#'
+                    to="#"
                     onClick={(e) => {
                         window.location = mailto;
                         e.preventDefault();
                     }}
                 >
-                    <button><strong>Hire me</strong></button>
+                    <button>
+                        <strong>Hire me</strong>
+                    </button>
                 </Link>
-                <Link to={cv}
-                    target="_blank"
-                    download='cv Marcin Fabisiak'>
-                    <button><strong>Download CV</strong></button>
+                <Link to={cv} target="_blank" download="cv Marcin Fabisiak">
+                    <button>
+                        <strong>Download CV</strong>
+                    </button>
                 </Link>
             </React.Fragment>
         );
@@ -49,22 +51,28 @@ const Upperpartanddownpart = ({ skillref, aboutref }) => {
     return (
         <div className="main-upperpartanddownpart col-12" ref={aboutref}>
             <div className="main-upperpart">
-                <div className="main-info" >
+                <div className="main-info">
                     <h2> // Hi, My name is Martin Fabisiak</h2>
                     <h3>Software Engineer</h3>
-                    <p>Passionate Techy and Tech Author<br />with 1
-                        year of experience within the field </p>
+                    <p>
+                        Passionate Techy and Tech Author
+                        <br />
+                        with 1 year of experience within the field{" "}
+                    </p>
                     <div className="main-info_seework">
-                        <p>See my work in github</p>
-                        <a href='https://github.com/marcinfabisiak97/one-page-portfolio-react/tree/dev'><img src={github} /></a>
-
+                        <a target='blank' href="https://github.com/marcinfabisiak97/one-page-portfolio-react/tree/dev">
+                            <p>See my work in github</p>
+                        </a>
+                        <a target='blank' href="https://github.com/marcinfabisiak97/one-page-portfolio-react/tree/dev">
+                            <img src={github} />
+                        </a>
                     </div>
                 </div>
                 <div className="main-photo_rightdescription">
                     <h1> // I am freelancer</h1>
                     <p>Contact me if you want to work with me</p>
                     <div>
-                        <ButtonMailto mailto={'mailto:marcinfabisiak123@gmail.com'} />
+                        <ButtonMailto mailto={"mailto:marcinfabisiak123@gmail.com"} />
                     </div>
                 </div>
             </div>
@@ -72,11 +80,12 @@ const Upperpartanddownpart = ({ skillref, aboutref }) => {
                 <div className="main-photo_belowdescription">
                     <h2 ref={skillref}> // About me</h2>
                     <h3>All about Techy</h3>
-                    <p>I am interested in practical creating innovative websites.
-                        I can easily make and maintain a full functional React sites.
-                        I always like to improve my skills and use new functionalities.
-                        I've recently started to improve make react with Typescript to make
-                        it more errorproof.
+                    <p>
+                        I am interested in practical creating innovative websites. I can
+                        easily make and maintain a full functional React sites. I always
+                        like to improve my skills and use new functionalities. I've recently
+                        started to improve make react with Typescript to make it more
+                        errorproof.
                     </p>
                     <h3>My interests</h3>
                     <ul>
@@ -88,21 +97,27 @@ const Upperpartanddownpart = ({ skillref, aboutref }) => {
                 <div className="main-wrapper_bars" ref={dref}>
                     <h2 id="skills">//Skills</h2>
                     <p>All beneath techs or languages are known by practical site.</p>
-                    {
-                        myBar.map((el, index) => {
-                            {/*making animation using css transform and hook useEffect and function handleScroll */ }
-                            return (
-                                <div className="wrapper-bar" >
-                                    <div key={index} style={{ width: `${(scrolled ? el.styl : "20%")}` }} className={el.klas}>
-                                        <p style={{ display: `${(scrolled ? 'block' : "none")}` }}>{el.text}</p>
-                                    </div>
+                    {myBar.map((el, index) => {
+                        {
+                            /*making animation using css transform and hook useEffect and function handleScroll */
+                        }
+                        return (
+                            <div className="wrapper-bar">
+                                <div
+                                    key={index}
+                                    style={{ width: `${scrolled ? el.styl : "20%"}` }}
+                                    className={el.klas}
+                                >
+                                    <p style={{ display: `${scrolled ? "block" : "none"}` }}>
+                                        {el.text}
+                                    </p>
                                 </div>
-                            )
-                        })
-                    }
+                            </div>
+                        );
+                    })}
                 </div>
             </div>
-        </div >
-    )
-}
+        </div>
+    );
+};
 export default Upperpartanddownpart;
