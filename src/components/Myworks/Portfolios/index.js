fd2ec9be7aa4ab49e8a3363a8e22a5d3231dc485
marcinfabisiak97 <marcinfab1@o2.pl>
2022-09-29 18:36:58 +0200

    changes styles

--- src/components/Myworks/Portfolios/index.js
+++ src/components/Myworks/Portfolios/index.js
@@ -2,24 +2,26 @@ import Images from '../../../assets/exportFiles';
 const Portfolios = () => {
     return (
         <div className="portfolios" >
-            <a target='blank' href='https://projekt2reactquiz.netlify.app'>
-                <div className="portfolios__view">
+
+            <div className="portfolios__view">
+                <a target='blank' href='https://projekt2reactquiz.netlify.app'>
                     <img src={Images.porfolio01} alt='porfolio' />
-                    <div className="portfolios__links">
-
-                        <a target='blank' href='https://github.com/marcinfabisiak97/quizReact'><img src={Images.gitHub} alt="github" /></a>
-                    </div>
+                </a>
+                <div className="portfolios__links">
+                    <a target='blank' href='https://github.com/marcinfabisiak97/quizReact'><img src={Images.gitHub} alt="github" /></a>
                 </div>
-            </a>
-            <a target='blank' href='https://cv-react-typescript-onepage-pr3.netlify.app/'>
-                <div className="portfolios__view">
+            </div>
+
+
+            <div className="portfolios__view">
+                <a target='blank' href='https://cv-react-typescript-onepage-pr3.netlify.app/'>
                     <img src={Images.porfolio02} alt='porfolio' />
-                    <div className="portfolios__links">
-
-                        <a target='blank' href='https://github.com/marcinfabisiak97/cv-projekt3-rem-Typescript'><img src={Images.gitHub} alt="github" /></a>
-                    </div>
+                </a>
+                <div className="portfolios__links">
+                    <a target='blank' href='https://github.com/marcinfabisiak97/cv-projekt3-rem-Typescript'><img src={Images.gitHub} alt="github" /></a>
                 </div>
-            </a>
+            </div>
+
         </div >
     )
 }
