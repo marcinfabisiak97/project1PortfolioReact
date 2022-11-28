b7416cb627dae1c4b58d94f96d58046f1c770a56
marcinfabisiak97 <marcinfab1@o2.pl>
2022-11-28 02:16:24 +0100

    change navigation adding animation

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -3,7 +3,6 @@ import { scrollToRef } from '../../components/Utils/Utils';
 import { GiHamburgerMenu } from 'react-icons/gi'
 import { TiDelete } from 'react-icons/ti'
 const Navigation = ({ aboutref, skillref, myworkref, blogref, contactref }) => {
-
     const navHeader = [
         { 'link': skillref, 'text': 'About me' },
         { 'link': myworkref, 'text': 'Portfolio' },
@@ -11,24 +10,23 @@ const Navigation = ({ aboutref, skillref, myworkref, blogref, contactref }) => {
         { 'link': contactref, 'text': 'Contact me' }
     ]
     const [isOpen, setOpen] = useState(true)
-    console.log(isOpen)
     return (
         <header className="header" ref={aboutref}>
-            <GiHamburgerMenu className={isOpen ? "header__burger" : "header__burger header__burger--invisible"} onClick={() => setOpen(!isOpen)} />
-            <TiDelete className={isOpen ? "header__burger header__burger--invisible" : "header__burger"} onClick={() => setOpen(!isOpen)} />
-
+            <div className='header__menuImg' >
+                <GiHamburgerMenu className={isOpen ? "header__burger" : "header__burger header__burger--invisible"} onClick={() => setOpen(!isOpen)} />
+                <TiDelete className={isOpen ? "header__burger header__burger--invisible" : "header__burger"} onClick={() => setOpen(!isOpen)} />
+            </div>
             <h3>M.F.</h3>
-            <article className="header__wrapper">
-                <nav className={isOpen ? 'header__nav header__nav--invisible' : 'header__nav'}>
-                    {
-                        navHeader.map((el, index) => {
-                            return (
-                                <div className='header__navLi' key={index}><a onClick={() => scrollToRef(el.link)}>{el.text}</a></div>
-                            )
-                        })
-                    }
-                </nav>
-            </article>
+            <nav className={isOpen ? 'header__nav header__nav--invisible' : 'header__nav'}>
+                {
+                    navHeader.map((el, index) => {
+                        return (
+                            <div className='header__navLi' key={index} onClick={() => scrollToRef(el.link)}><div>{el.text}</div></div>
+                        )
+                    })
+                }
+            </nav>
+
 
         </header>
     )
